11ef29e22d30057a7d299e2d1f8c0215faf4f3d9
Poojan Shrestha <poojan.shrestha@multiscale.io>
2021-10-29 13:22:47 -0700

    Added project KenSci.Setup.DataLoader.

---
+++ KenSci.Setup.DataLoader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KenSci.Setup.DataLoader
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Agent Ken V3");
+        }
+    }
+}
