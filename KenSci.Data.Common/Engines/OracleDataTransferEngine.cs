285bafbf31a7d342cf4080be52fd53bab7dd42e2
Poojan Shrestha <poojan.shrestha@multiscale.io>
2021-10-29 15:19:27 -0700

    Init2

--- KenSci.Data.Common/Engines/OracleDataTransferEngine.cs
+++ KenSci.Data.Common/Engines/OracleDataTransferEngine.cs
@@ -1,12 +1,28 @@
 using System;
+using System.Configuration;
 
 namespace KenSci.Data.Common.Engines
 {
     public class OracleDataTransferEngine
     {
+
+        private string GetOracleConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["OracleConnectionString"].ConnectionString;
+        }
+        
+        private string GetSqlServerConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["SqlServerConnectionString"].ConnectionString;
+        }
         public void SayGoodbye()
         {
             Console.WriteLine("Say Goodbye!!");
+            var config = ConfigurationManager.ConnectionStrings["OracleConnectionString"];
+            // Console.WriteLine(GetOracleConnectionString());
+            // Console.WriteLine(GetSqlServerConnectionString());
+
+            Console.WriteLine(config);
         }
     }
 }
