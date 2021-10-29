a7c4b5b56526d465de936237ea102a2fc7b7995a
Poojan Shrestha <poojan.shrestha@multiscale.io>
2021-10-29 13:39:46 -0700

    Added KenSci.Data.Common.

---
+++ KenSci.Data.Common/Engines/OracleDataTransferEngine.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace KenSci.Data.Common.Engines
+{
+    public class OracleDataTransferEngine
+    {
+        public void SayGoodbye()
+        {
+            Console.WriteLine("Say Goodbye!!");
+        }
+    }
+}
--- KenSci.Setup.DataLoader/Program.cs
+++ KenSci.Setup.DataLoader/Program.cs
@@ -6,7 +6,10 @@ namespace KenSci.Setup.DataLoader
     {
         public static void Main(string[] args)
         {
+
+            var engine = new KenSci.Data.Common.Engines.OracleDataTransferEngine();
             Console.WriteLine("Agent Ken V3");
+            engine.SayGoodbye();
         }
     }
 }
