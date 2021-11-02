74830f7046fdb7b277f6e55bf995f87322c648a0
Poojan Shrestha <poojan.shrestha@multiscale.io>
2021-11-02 07:33:28 -0700

    Fetch config data.

--- KenSci.Setup.DataLoader/Program.cs
+++ KenSci.Setup.DataLoader/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using KenSci.Data.Common.Contracts.DTO;
 using KenSci.Data.Common.Engines;
@@ -16,7 +18,56 @@ namespace KenSci.Setup.DataLoader
             Console.WriteLine("Agent Ken V3");
             // engine.Import();
 
-            var sqlConnectionString = engine.GetSqlConnectionString();
+            // var sqlConnectionString = engine.GetSqlConnectionString();
+            var sqlConnectionString =
+                "Data Source=localhost;Initial Catalog=AgentKenConfig;User ID=sa;Password=Pass123!;Connection Timeout=3000";
+            
+            Console.WriteLine(sqlConnectionString);
+
+            
+            using (var connection = new SqlConnection(sqlConnectionString))
+            {
+                Console.WriteLine(connection);
+                string sql = "select * from KSAgent_Config_Tbl";
+                using (var da = new SqlDataAdapter(sql, connection))
+                {
+                    var tblConfig = new DataTable();
+                    try
+                    {
+                        connection.Open();
+                        da.Fill(tblConfig);
+                        var row = tblConfig.Rows[0];
+                        
+                        var tableSchema = row.Field<string>("TableSchema");
+                        var tableName = row.Field<string>("TableName");
+                        var sourceDb = row.Field<string>("SourceDb");
+                        var sourceServer = row.Field<string>("SourceServer");
+                        var destinationSchema = row.Field<string>("DestinationSchema");
+                        
+                        Console.WriteLine("SourceServer: {0}, SourceDb: {1}", sourceServer, sourceDb);
+                        Console.WriteLine("TableSchema: {0}, TableName: {1}", tableSchema, tableName);
+                        Console.WriteLine("DestinationSchema: {0}", destinationSchema);
+                    }
+                    catch (Exception ex)
+                    {
+                        throw;
+                    }
+                }
+                // var command = new SqlCommand(sql, connection);
+                //     command.CommandTimeout = 3600;
+                //     command.CommandType = CommandType.Text;
+                //     var sqlDataReader = command.ExecuteReader();
+                //
+                //     while (sqlDataReader.Read())
+                //     {
+                //         Console.WriteLine(sqlDataReader[0]);
+                //     }
+                //     
+                //     sqlDataReader.Close();
+            }
+            
+            
+            
 
             // ServerConnection serverConnection = new ServerConnection(sqlConnectionString);
             
