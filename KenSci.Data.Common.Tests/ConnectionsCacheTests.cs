435f7ae08ba4ccc4618d27c43aa311f86852e01b
Poojan Shrestha <poojan.shrestha@multiscale.io>
2021-11-10 10:42:28 -0800

    Added ConnectionsCache. Initialized test framework.

---
+++ KenSci.Data.Common.Tests/ConnectionsCacheTests.cs
@@ -0,0 +1,33 @@
+using KenSci.Data.Common.Singletons;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KenSci.Data.Common.Tests
+{
+    [TestClass]
+    public class ConnectionsCacheTests
+    {
+        private readonly ConnectionsCache _connectionsCache;
+
+        public ConnectionsCacheTests()
+        {
+            _connectionsCache = ConnectionsCache.GetInstance;    
+            _connectionsCache.Add("TestKey1", "TestValue1");
+            _connectionsCache.Add("TestKey2", "TestValue2");
+        }
+        
+        [TestMethod]
+        public void ShouldContainInitialValues()
+        {
+            Assert.AreEqual(2, _connectionsCache.GetCount());
+            Assert.AreEqual("TestValue1", _connectionsCache.GetOrNull("TestKey1"));
+            Assert.AreEqual("TestValue2", _connectionsCache.GetOrNull("TestKey2"));
+        }
+
+        [TestMethod]
+        public void ShouldAddAndGetValues()
+        {
+            _connectionsCache.Add("MyNumber", 42);
+            Assert.AreEqual(42, (int) _connectionsCache.GetOrNull("MyNumber"));
+        }
+    }
+}
---
+++ KenSci.Data.Common/Singletons/ConnectionsCache.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections.Generic;
+
+namespace KenSci.Data.Common.Singletons
+{
+    public class ConnectionsCache
+    {
+        
+        private readonly IDictionary<string, object> _cacheDictionary;
+        
+        private static ConnectionsCache _instance = null;
+
+        public static ConnectionsCache GetInstance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new ConnectionsCache();
+                }
+
+                return _instance;
+            }
+        }
+
+        private ConnectionsCache()
+        {
+            _cacheDictionary = new Dictionary<string, object>();
+        }
+        
+        public virtual void Add(string key, object value)
+        {
+            lock (_cacheDictionary)
+            {
+                _cacheDictionary[key] = value;
+            }
+        }
+
+        public virtual object GetOrNull(string key)
+        {
+            lock (_cacheDictionary)
+            {
+                if (_cacheDictionary.TryGetValue(key, out object value))
+                {
+                    return value;
+                }
+
+                return null;
+            }
+        }
+
+        public virtual object GetOrAdd(string key, Func<object> factory)
+        {
+            lock (_cacheDictionary)
+            {
+                var value = GetOrNull(key);
+                if (value != null)
+                {
+                    return value;
+                }
+
+                value = factory();
+                Add(key, value);
+
+                return value;
+            }
+        }
+
+        public virtual void Clear()
+        {
+            lock (_cacheDictionary)
+            {
+                _cacheDictionary.Clear();
+            }
+        }
+
+        public virtual bool Remove(string key)
+        {
+            lock (_cacheDictionary)
+            {
+                return _cacheDictionary.Remove(key);
+            }
+        }
+
+        public virtual int GetCount()
+        {
+            lock (_cacheDictionary)
+            {
+                return _cacheDictionary.Count;
+            }
+        }
+    }
+}
