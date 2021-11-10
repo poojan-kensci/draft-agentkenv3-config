19c96f640f1fe65c53cc0e29427947f8392dd251
Poojan Shrestha <poojan.shrestha@multiscale.io>
2021-11-09 16:03:32 -0800

    Clean up.

--- KenSci.Data.Common/Engines/OracleDataTransfer.cs
+++ KenSci.Data.Common/Engines/OracleDataTransfer.cs
@@ -1,12 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Linq;
 using System.Text;
 using KenSci.Data.Common.Contracts;
 using KenSci.Data.Common.Helpers;
 using System.Data.SqlClient;
-using NLog;
 using Oracle.ManagedDataAccess.Client;
 
 namespace KenSci.Data.Common.Engines
